af57ba1a455a6e6bb20be92cb6051e3e43d036f4
Maxwell Dantas <mdszdev@gmail.com>
2021-09-26 23:04:31 -0300

    Exercise 1059 done

---
+++ src/main/java/uri/exercises/iniciante/Exercise1059.java
@@ -0,0 +1,15 @@
+package uri.exercises.iniciante;
+
+import java.io.IOException;
+
+public class Exercise1059 {
+
+    public static void main(String[] args) throws IOException {
+
+        for (int x = 1; x <= 100; x++) {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+}
